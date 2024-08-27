409646b0815556485c7e3529951b7847541d5698
PierluigiPapa <pierluigipapa9@gmail.com>
2024-08-27 20:24:43 +0200

    feat: added new column intervento_gratuito on tipi_intervento's table

--- BackEnd/database/migrations/2024_08_26_165407_create_tipi_intervento_table.php
+++ BackEnd/database/migrations/2024_08_26_165407_create_tipi_intervento_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('tipi_intervento', function (Blueprint $table) {
             $table->id();
             $table->string('tipologia', 255);
+            $table->boolean('intervento_gratuito')->default(true);
             $table->string('dettagli', 255);
             $table->timestamps();
         });
